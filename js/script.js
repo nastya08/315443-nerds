f0b4717f6ca7ed98b275b0938010b2697d5675a4
nastya08 <nastyaivanova27@gmail.com>
2017-02-16 22:01:21 +0300

    Финальная проверка

--- js/script.js
+++ js/script.js
@@ -1,7 +1,12 @@
 var link = document.querySelector(".btn-write-us");
 
 var popup = document.querySelector(".modal-window");
-var close = popup.querySelector(".modal-window-close")
+var close = popup.querySelector(".modal-window-close");
+
+var form = popup.querySelector("form");
+var login = popup.querySelector("[name=name]");
+var email = popup.querySelector("[name=email]");
+var letter = popup.querySelector("[name=letter]");
 
 link.addEventListener("click", function(event) {
   event.preventDefault();
@@ -11,4 +16,11 @@ link.addEventListener("click", function(event) {
 close.addEventListener("click", function(event) {
   event.preventDefault();
   popup.classList.remove("modal-window-show");
+})
+
+form.addEventListener("submit", function(event) {
+  if (!login.value || !email.value || !letter.value) {
+    event.preventDefault();
+    console.log("Необходимо заполнить все поля");
+  }
 })
